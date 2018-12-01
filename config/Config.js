9174d12698472e43870c95b617d386283d0efe65
ShermanTheCat <40065490+mjkelley354@users.noreply.github.com>
2018-12-01 11:05:17 -0500

    sample

---
+++ config/Config.js
@@ -0,0 +1,10 @@
+const Config = {};
+Config.dbHost = process.env.DB_HOST;
+Config.dbPort = process.env.DB_PORT;
+Config.dbUser = process.env.DB_USER;
+Config.dbPassword = process.env.DB_PASSWORD;
+
+module.exports = Config;
+
+
+
