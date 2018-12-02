d97401b49a9e8098613d471a2a47463c46371859
ShermanTheCat <40065490+mjkelley354@users.noreply.github.com>
2018-12-02 06:24:08 -0500

    updated schema models (but it will still probably require more

--- config/Config.js
+++ config/Config.js
@@ -1,10 +1,8 @@
-const Config = {};
-Config.dbHost = process.env.DB_HOST;
-Config.dbPort = process.env.DB_PORT;
-Config.dbUser = process.env.DB_USER;
-Config.dbPassword = process.env.DB_PASSWORD;
+const Config = {
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD
+};
 
 module.exports = Config;
-
-
-
