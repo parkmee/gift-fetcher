3d5ec4a9c3cd82f19d371d7477a470272ec0070b
ShermanTheCat <40065490+mjkelley354@users.noreply.github.com>
2018-12-08 13:15:24 -0500

    updated html routes with some defaults for testing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,18 @@
-const db = require("../models");
+const path = require("path");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+
+  app.get("/logon", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/html/logon.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/profile", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/html/new-user.html"));
+  });
+
+  app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/html/new-index.html"));
   });
 
   // Render 404 page for any unmatched routes
