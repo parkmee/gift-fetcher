bba9f48921dd14d25fbd738e7265065c59f609cf
MMPark <parkmee@gmail.com>
2018-12-09 17:48:36 -0500

    fixed linting

--- public/js/testdata.js
+++ public/js/testdata.js
@@ -22,35 +22,40 @@ const testEvents = [
     eventTitle: "Wedding",
     eventDate: "12/25/2018",
     eventPurchased: false
-  }, {
+  },
+  {
     personFirstName: "Meeyoung",
     personLastName: "Park",
     eventCreatedBy: 1,
     eventTitle: "Christmas",
     eventDate: "12/25/2018",
     eventPurchased: true
-  }, {
+  },
+  {
     personFirstName: "Natalie",
     personLastName: "Portman",
     eventCreatedBy: 1,
     eventTitle: "Hanukkah",
     eventDate: "12/25/2018",
     eventPurchased: true
-  }, {
+  },
+  {
     personFirstName: "Nina",
     personLastName: "Dobrev",
     eventCreatedBy: 1,
     eventTitle: "Custom Event",
     eventDate: "12/25/2018",
     eventPurchased: false
-  }, {
+  },
+  {
     personFirstName: "Dave",
     personLastName: "Zee",
     eventCreatedBy: 1,
     eventTitle: "Graduation",
     eventDate: "12/25/2018",
     eventPurchased: true
-  }, {
+  },
+  {
     personFirstName: "Rupesh",
     personLastName: "Srinivasan",
     eventCreatedBy: 1,
