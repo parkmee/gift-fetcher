95c561011f5aa5ffcd137b67aa534928e2afd345
MMPark <parkmee@gmail.com>
2018-12-09 10:47:07 -0500

    updated html routes, enabled server access to static content

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,10 @@ const path = require("path");
 module.exports = function(app) {
   // Load index page
 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/html/index.html"));
+  });
+
   app.get("/logon", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/html/logon.html"));
   });
--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
-
+app.use("/public", express.static(__dirname + "/public"));
 // Handlebars
 app.engine(
   "handlebars",
