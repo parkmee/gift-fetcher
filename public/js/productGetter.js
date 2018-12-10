657c6225b77c9430d5b984b016878a6c8c9eefd6
MMPark <parkmee@gmail.com>
2018-12-10 16:31:58 -0500

    enabled product search via html route

--- public/js/productGetter.js
+++ public/js/productGetter.js
@@ -1,8 +1,8 @@
 //dependencies
-const request = require('request');
-const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 
-const searchProducts = function (searchTerms, itemNumber) {
+module.exports = function(searchTerms, itemNumber) {
   let products = [];
 
   // Join search string with '+' sign
@@ -13,68 +13,69 @@ const searchProducts = function (searchTerms, itemNumber) {
   const queryUrl = `https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=${searchString}&tag=giftfetcher-20&sort=review-rank`;
 
   // get DOM content
-  request(queryUrl, function (error, response, body) {
-    if (error) {
-      return console.error(error);
-    }
+  request(queryUrl, function(error, response, body) {
+    setTimeout(() => {
+      if (error) {
+        return console.error(error);
+      }
 
-    if (response.statusCode === 200) {
-      const $ = cheerio.load(body);
+      if (response.statusCode === 200) {
+        const $ = cheerio.load(body);
 
-      const getASIN = () => {
-        return $("#result_" + i)[0].attribs["data-asin"];
-      };
-
-      let title = "";
-      const getLongTitle = () => {
-        title = $("#result_" + i + " .a-link-normal > h2")[0].attribs["data-attribute"];
-        return title;
-      };
-
-      const getShortTitle = () => {
-        if (title.length > 50) {
-          return title.slice(0, 80) + "...";
-        } else {
-          return title;
-        }
-      };
-
-      const getProductUrl = () => {
-        const productUrl = $("#result_" + i + " .a-link-normal")[0].attribs.href;
-        return productUrl + "&tag=giftfetcher-20";
-      };
-
-      const getProductImage = () => {
-        return $("#result_" + i + " .a-link-normal")[0].children[0].attribs.src;
-      };
-
-      const getPrice = () => {
-        const priceWhole = $("#result_" + i + " .sx-price-whole").text();
-        const priceFraction = $("#result_" + i + " .sx-price-fractional").text();
-        return parseFloat(priceWhole) + parseFloat(priceFraction / 100);
-      };
-
-      for (i = 0; i < itemNumber; i++) {
-        let product = {
-          asin: getASIN(),
-          description: getLongTitle(),
-          title: getShortTitle(),
-          productUrl: getProductUrl(),
-          imageUrl: getProductImage(),
-          price: getPrice()
+        const getASIN = () => {
+          return $("#result_" + i)[0].attribs["data-asin"];
         };
 
-        products.push(product);
+        let title = "";
+        const getLongTitle = () => {
+          title = $("#result_" + i + " .a-link-normal > h2")[0].attribs["data-attribute"];
+          return title;
+        };
+
+        const getShortTitle = () => {
+          if (title.length > 50) {
+            return title.slice(0, 80) + "...";
+          } else {
+            return title;
+          }
+        };
+
+        const getProductUrl = () => {
+          const productUrl = $("#result_" + i + " .a-link-normal")[0].attribs.href;
+          return productUrl + "&tag=giftfetcher-20";
+        };
+
+        const getProductImage = () => {
+          return $("#result_" + i + " .a-link-normal")[0].children[0].attribs.src;
+        };
+
+        const getPrice = () => {
+          const priceWhole = $("#result_" + i + " .sx-price-whole").text();
+          const priceFraction = $("#result_" + i + " .sx-price-fractional").text();
+          return parseFloat(priceWhole) + parseFloat(priceFraction / 100);
+        };
+
+        for (i = 3; i < itemNumber + 4; i++) {
+          let product = {
+            asin: getASIN(),
+            description: getLongTitle(),
+            title: getShortTitle(),
+            productUrl: getProductUrl(),
+            imageUrl: getProductImage(),
+            price: getPrice()
+          };
+
+          products.push(product);
+        }
+        console.log(products);
+        return products;
       }
-      console.log(products);
-      return products;
-    }
+    }, 3000);
   });
 };
 
-searchProducts("pink blanket", 3);
-
-module.exports = searchProducts;
+//test
+//searchProducts("pink blanket", 3);
 
 // alternate option - does not return all required variables
 
